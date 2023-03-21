3a74fdfb7ec4b006dfc4c0fa7583f4a63a0e85a5
Mario Cao <mariocao@gmail.com>
2023-03-21 12:39:16 +0100

    refactor(crypto): add tests and clean code

--- crypto/src/crypto_test.rs
+++ crypto/src/crypto_test.rs
@@ -4,6 +4,7 @@ use bn254::ECDSA;
 use ed25519_dalek::{Keypair, SecretKey, Signature, Signer};
 
 use super::*;
+use crate::{CryptoError, MasterKey};
 
 const TEST_SK_PATH: &str = "./seda_test_sk";
 fn generate_test_sk() {
@@ -31,4 +32,33 @@ fn generate_ed25519_pair() {
     let msg: &[u8] = b"awesome-seda";
     let signature: Signature = dalek_pair.sign(msg);
     assert!(dalek_pair.verify(msg, &signature).is_ok());
+}
+
+#[test]
+fn master_key_from_hex_1() {
+    let mk_random = MasterKey::random();
+    let mk_string: String = mk_random.into();
+    let mk_from_string = MasterKey::try_from(mk_string);
+    assert!(mk_from_string.is_ok());
+}
+
+#[test]
+fn master_key_from_hex_2() {
+    let mk_string: String = "0000000000000000000000000000000000000000000000000000000000000000".to_string();
+    let master_key = MasterKey::try_from(mk_string);
+    assert!(master_key.is_ok());
+}
+
+#[test]
+fn master_key_from_hex_error_length() {
+    let mk_string: String = "1234".to_string();
+    let master_key = MasterKey::try_from(mk_string);
+    assert!(matches!(master_key, Err(CryptoError::InvalidMasterKeyLength(_))));
+}
+
+#[test]
+fn master_key_from_hex_error_invalid() {
+    let mk_string: String = "potato".to_string();
+    let master_key = MasterKey::try_from(mk_string);
+    assert!(matches!(master_key, Err(CryptoError::FromHex(_))));
 }
